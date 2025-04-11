d26951a3e7b08d835576e259fac3b38cbad1a312
Anup Kumar <anup28kumar@outlook.com>
2025-04-11 10:36:08 +0300

    Billing service added GRPC connection established between patient and billing service

--- patient-service/src/main/java/com/ak/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/ak/patientservice/controller/PatientController.java
@@ -3,6 +3,8 @@ package com.ak.patientservice.controller;
 import com.ak.patientservice.dto.PatientRequestDto;
 import com.ak.patientservice.dto.PatientResponseDto;
 import com.ak.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/patients")
+@Tag(name = "Patient", description = "API for managing Patients")
 public class PatientController {
 
   private final PatientService patientService;
@@ -23,11 +26,13 @@ public class PatientController {
   }
 
   @GetMapping
+  @Operation(summary = "Get Patients")
   public List<PatientResponseDto> getPatients() {
     return patientService.findAll();
   }
 
   @PostMapping
+  @Operation(summary = "Create a new Patient")
   public PatientResponseDto createPatient(@Valid @RequestBody PatientRequestDto dto) {
     return patientService.createPatient(dto);
   }
--- patient-service/src/main/java/com/ak/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/ak/patientservice/service/PatientService.java
@@ -3,6 +3,7 @@ package com.ak.patientservice.service;
 import com.ak.patientservice.dto.PatientRequestDto;
 import com.ak.patientservice.dto.PatientResponseDto;
 import com.ak.patientservice.exceptions.EmailAlreadyExistsException;
+import com.ak.patientservice.grpc.BillingServiceGrpcClient;
 import com.ak.patientservice.mapper.PatientMapper;
 import com.ak.patientservice.model.Patient;
 import com.ak.patientservice.repository.PatientRepository;
@@ -15,9 +16,11 @@ import java.util.stream.Collectors;
 public class PatientService {
 
   private final PatientRepository patientRepository;
+  private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-  public PatientService(PatientRepository patientRepository) {
+  public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
     this.patientRepository = patientRepository;
+    this.billingServiceGrpcClient = billingServiceGrpcClient;
   }
 
   public List<PatientResponseDto> findAll() {
@@ -29,6 +32,9 @@ public class PatientService {
       throw new EmailAlreadyExistsException("Patient already exists with this email :: " + dto.getEmail());
     }
     Patient patient = patientRepository.save(PatientMapper.toPatientModel(dto));
+
+    billingServiceGrpcClient.createBillingAccount(patient.getId().toString(),patient.getName(),patient.getEmail());
+
     return PatientMapper.patientToPatientResponseDto(patient);
   }
 }
