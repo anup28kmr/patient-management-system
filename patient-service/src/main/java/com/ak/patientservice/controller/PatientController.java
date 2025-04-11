50f6ec39dd15d4f2ae21519433ef62128cde1113
Anup Kumar <anup28kumar@outlook.com>
2025-04-11 16:52:53 +0300

    api-gateway and analytics services added along with kafka

--- patient-service/src/main/java/com/ak/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/ak/patientservice/controller/PatientController.java
@@ -6,6 +6,8 @@ import com.ak.patientservice.service.PatientService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/patients")
+@RequestMapping("/v1/patients")
 @Tag(name = "Patient", description = "API for managing Patients")
 public class PatientController {
 
+  private static final Logger log = LoggerFactory.getLogger(PatientController.class);
   private final PatientService patientService;
 
   public PatientController(PatientService patientService) {
@@ -34,6 +37,7 @@ public class PatientController {
   @PostMapping
   @Operation(summary = "Create a new Patient")
   public PatientResponseDto createPatient(@Valid @RequestBody PatientRequestDto dto) {
+    log.info("Request received to create a new patient with email :: {}", dto.getEmail());
     return patientService.createPatient(dto);
   }
 }
--- patient-service/src/main/java/com/ak/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/ak/patientservice/service/PatientService.java
@@ -4,9 +4,12 @@ import com.ak.patientservice.dto.PatientRequestDto;
 import com.ak.patientservice.dto.PatientResponseDto;
 import com.ak.patientservice.exceptions.EmailAlreadyExistsException;
 import com.ak.patientservice.grpc.BillingServiceGrpcClient;
+import com.ak.patientservice.kafka.KafkaProducer;
 import com.ak.patientservice.mapper.PatientMapper;
 import com.ak.patientservice.model.Patient;
 import com.ak.patientservice.repository.PatientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +18,15 @@ import java.util.stream.Collectors;
 @Service
 public class PatientService {
 
+  private static final Logger log = LoggerFactory.getLogger(PatientService.class);
   private final PatientRepository patientRepository;
   private final BillingServiceGrpcClient billingServiceGrpcClient;
+  private final KafkaProducer kafkaProducer;
 
-  public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
+  public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer) {
     this.patientRepository = patientRepository;
     this.billingServiceGrpcClient = billingServiceGrpcClient;
+    this.kafkaProducer = kafkaProducer;
   }
 
   public List<PatientResponseDto> findAll() {
@@ -29,12 +35,14 @@ public class PatientService {
 
   public PatientResponseDto createPatient(PatientRequestDto dto) {
     if(patientRepository.existsByEmail(dto.getEmail())) {
+      log.error("Patient with email : {} already exists", dto.getEmail());
       throw new EmailAlreadyExistsException("Patient already exists with this email :: " + dto.getEmail());
     }
-    Patient patient = patientRepository.save(PatientMapper.toPatientModel(dto));
+    Patient newPatient = patientRepository.save(PatientMapper.toPatientModel(dto));
 
-    billingServiceGrpcClient.createBillingAccount(patient.getId().toString(),patient.getName(),patient.getEmail());
+    billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),newPatient.getName(),newPatient.getEmail());
 
-    return PatientMapper.patientToPatientResponseDto(patient);
+    kafkaProducer.sendEvent(newPatient);
+    return PatientMapper.patientToPatientResponseDto(newPatient);
   }
 }
